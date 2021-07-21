b11d6fb05901e3861438b2c2fa960a43016b55f0
joneskj55 <joneskj55@gmail.com>
2021-07-21 14:02:50 -0500

    add services to array & return

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { IService } from './service.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServicesService {
+  services: Array<IService>; // services array
 
-  constructor() { }
+  constructor() {
+    // store list of services in an array
+    this.services = [
+      {
+        name: 'Password Reset',
+        price: '39.99',
+        checked: false,
+      },
+      {
+        name: 'Spyware Removal',
+        price: 99.99,
+        checked: false,
+      },
+      {
+        name: 'RAM Upgrade',
+        price: 129.99,
+        checked: false,
+      },
+      {
+        name: 'Software Installation',
+        price: 49.99,
+        checked: false,
+      },
+      {
+        name: 'Tune-up',
+        price: 89.99,
+        checked: false,
+      },
+      {
+        name: 'Keyboard Cleaning',
+        price: 45.00,
+        checked: false,
+      },
+      {
+        name: 'Disk Clean-up',
+        price: 149.99,
+        checked: false,
+      },
+    ];
+
+    // get the list of services
+    getServices() {
+      return this.services;
+  }
 }
